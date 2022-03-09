2ef2bc112d9247078086c2e3ae5c12e917a93f11
Melvyn27 <melvyn2701@gmail.com>
2022-03-09 09:16:39 +0100

    add: test

---
+++ Test.java
@@ -0,0 +1,17 @@
+public class Test {
+    private int test;
+    private int bg;
+
+    public Test(int test, int bg) {
+        this.test = test;
+        this.bg = bg;
+    }
+
+    public int getTest() {
+        return test;
+    }
+
+    public void setTest(int test) {
+        this.test = test;
+    }
+}
