992675689934e6b52185eb0aa56d3f14991394c0
Guilhem <guilhem.cros.38@gmail.com>
2022-03-18 16:55:49 +0100

    placement des principals panneau

--- graphics/Screen.java
+++ graphics/Screen.java
@@ -2,6 +2,7 @@ package SAE.graphics;
 
 import SAE.graphics.screen_component.ChoixSite;
 import SAE.graphics.screen_component.Log;
+import SAE.graphics.screen_component.Questionnement;
 import SAE.map.Site;
 
 import javax.swing.*;
@@ -10,31 +11,60 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.ArrayList;
 
-public class Screen extends JFrame implements ActionListener{
+public class Screen extends JFrame implements ActionListener, ComponentListener, WindowListener {
     ArrayList<Site> sites = new ArrayList<>();
-    int SelectedSite1;
-    Log log = new Log(0,0,100,100);
-    ChoixSite site1=new ChoixSite(0,100,100,100);
 
+    Log log = new Log();
+    ChoixSite site1=new ChoixSite();
+    ChoixSite site2=new ChoixSite();
+    Questionnement quest = new Questionnement();
 
 
     public Screen(){
 
-        //TODO creation de la fenetre
+        //TODO set la fenetre
         super("graph-Map");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
         setSize(800,400);
         setLayout(null);
+        setVisible(true);
+        addWindowListener(this);
+        addComponentListener(this);
         //todo ajout des composant
         add(site1.getView());
-        //add(log.getView());
-        //for(int i=0;i<200;i++)log.addLine("test");
+        add(site2.getView());
+        add(log.getView());
+        add(quest.getView());
 
 
-        //TODO afficher la fenetre
-        setVisible(true);
+        //todo set les parametre composant
+        quest.link(this);
+        placeComponent();
+
+
+
     }
+    void init(){
+
+
+    }
+
+    /**
+     * move component when window resize
+     */
+    void placeComponent(){
+        int a=(int) ((getHeight()-20) * 0.70);
+        int b=(int) ((getHeight()-20) * 0.30);
+        int c=(int) (getWidth() * 0.70);
+
+        site1.updateLocation(0,0,100,a);
+        site2.updateLocation(100,0,100,a);
+        quest.updateLocation(0,a,200,b);
+        log.updateLocation(200,a,200,b);
+
+
+    }
+
+
 
     public void mainScreen(){
 
@@ -42,9 +72,72 @@ public class Screen extends JFrame implements ActionListener{
     }
 
 
+    public void action(){
+        log.addLine(""+site1.getChoix());
+
+
+
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
-        System.out.println(e.getActionCommand());
+
+    }
+
+    @Override
+    public void windowOpened(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowClosing(WindowEvent e) {
+        System.exit(0);
+    }
+
+    @Override
+    public void windowClosed(WindowEvent e) {
+        System.exit(0);
+    }
+
+    @Override
+    public void windowIconified(WindowEvent e) {
+
+    }
+
+
+    @Override
+    public void windowDeiconified(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowActivated(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent e) {
+
+    }
+
+    @Override
+    public void componentResized(ComponentEvent e) {
+        placeComponent();
+    }
+
+    @Override
+    public void componentMoved(ComponentEvent e) {
+
+    }
+
+    @Override
+    public void componentShown(ComponentEvent e) {
+
+    }
+
+    @Override
+    public void componentHidden(ComponentEvent e) {
+
     }
 }
 
@@ -54,10 +147,10 @@ public class Screen extends JFrame implements ActionListener{
  * fait
  * - creation de deux panneaux d'affichage pour le choix des sites
  * - creation de un panneau de Log pour afficher les resultats
- *
+ * - class de questionement du graph
  * a faire:
  * - finaliser les panneaux d'affichage
- * - class de questionement du graph
+ * - finaliser de questionement du graph pour la mise a jour d'action
  * - class de dessin pour afficher le graph dynamiquement
- * - creer un affichage dynamique avce un WindowsListener
+ * - creer un affichage dynamique
  */
