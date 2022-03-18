e7ff5dc8eacf8bd31cdf8f5895a3095c90ceb605
Guilhem <guilhem.cros.38@gmail.com>
2022-03-18 16:56:17 +0100

    nothing

---
+++ graphics/DisplayTest.java
@@ -0,0 +1,21 @@
+package SAE.graphics;
+
+import SAE.graphics.screen_component.Log;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class DisplayTest {
+
+
+
+
+    public static void main(String argv[]) {
+        JFrame mainScreen = new Screen();
+
+
+    }
+
+
+
+}
