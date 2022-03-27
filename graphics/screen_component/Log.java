1e91c2b99413cead969b3bcae1984db03ff056a3
Sigma <59624364+androlink@users.noreply.github.com>
2022-03-27 17:42:46 +0200

    correction du systeme d'affichage

--- graphics/screen_component/Log.java
+++ graphics/screen_component/Log.java
@@ -4,11 +4,6 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Log extends JPanel {
-    int x=0;
-    int y=0;
-    int width =10;
-    int height =10;
-    int length=0;
     JScrollPane view;
 
 
@@ -16,45 +11,29 @@ public class Log extends JPanel {
         init();
     }
 
-    public Log(int x, int y) {
-        this.x = x;
-        this.y = y;
-        init();
-    }
 
-    public Log(int x, int y, int width, int height) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        init();
-    }
+
+
     void init(){
         setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
         view = new JScrollPane(this);
-        setBounds(0,0,this.getWidth(),0);
-        view.setBounds(x,y,width,height);
+
 
     }
     public void addLine(String str){
         add(new JLabel(str));
-        setSize(width,getComponentCount()*25);
+        setSize(this.getWidth(),getComponentCount()*25);
+        System.out.println("new log added : " + str);
+        JScrollBar bar= view.getVerticalScrollBar();
+        bar.setValue(bar.getMaximum()+1000);
     }
-    public void updateLocation(int x,int y,int width,int height){
-        this.x=x;
-        this.y=y;
-        this.width=width;
-        this.height=height;
-        view.setBounds(x,y,width,height);
+    public void clear(){
+        removeAll();
+        setSize(this.getWidth(),getComponentCount()*25);
+        System.out.println("component free");
     }
-    public void setWidth(int width){
-        this.width=width;
-        view.setBounds(x,y,width,height);
-    }
-    public void setHeight(int height){
-        this.height=height;
-        view.setBounds(x,y,width,height);
-    }
+
+
 
 
     public JScrollPane getView() {
