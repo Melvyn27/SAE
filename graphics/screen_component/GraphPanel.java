00565a0d0eb92edd4915e25f742c9ccaf20f7b61
Guilhem <guilhem.cros.38@gmail.com>
2022-03-28 17:26:43 +0200

    initialisation du panneau qui contiendra le graph

---
+++ graphics/screen_component/GraphPanel.java
@@ -0,0 +1,23 @@
+package SAE.graphics.screen_component;
+
+import SAE.map.Carte;
+
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class GraphPanel extends JPanel {
+    Carte carte;
+
+
+
+
+    public void setCarte(Carte carte){
+        this.carte=carte;
+    }
+
+
+
+
+
+
+}
