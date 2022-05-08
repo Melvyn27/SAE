9f0a60249077094890e2f5563200eeaf7a328f74
guilhem <guilhem.cros.38@gmail.com>
2022-05-08 23:05:47 +0200

    changement et finialisation du gestionnaire d'affichage

--- graphics/screen_component/Log.java
+++ graphics/screen_component/Log.java
@@ -2,9 +2,12 @@ package SAE.graphics.screen_component;
 
 import javax.swing.*;
 import java.awt.*;
+import java.lang.reflect.Parameter;
+import java.net.NetworkInterface;
 
 public class Log extends JPanel {
     JScrollPane view;
+
 
 
     public Log() {
@@ -21,24 +24,25 @@ public class Log extends JPanel {
 
     }
     public void addLine(String str){
-        //fixme affichage a refaire
+
         //this.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
         add(new JLabel(str));
         //setSize(this.getWidth(),getComponentCount()*25);
-        System.out.println("new log added : " + str);
+        //System.out.println("new log added : " + str);
+        revalidate();
         JScrollBar bar= view.getVerticalScrollBar();
+        //fixme bug de scrollBar
         bar.setValue(bar.getMaximum());
-
+        view.revalidate();
     }
+
+
     public void clear(){
         removeAll();
         //setSize(this.getWidth(),getComponentCount()*25);
         System.out.println("component free");
+        revalidate();
     }
-
-
-
-
     public JScrollPane getView() {
         return view;
     }
