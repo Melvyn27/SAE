81eb4a6ca4deb5d1f9999bab03d6a88f39eaeeb7
guilhem <guilhem.cros.38@gmail.com>
2022-05-19 19:55:17 +0200

    ajout de model de test de lancement parallel

--- Test.java
+++ Test.java
@@ -1,6 +1,10 @@
 
 package SAE;
 
+import com.sun.source.util.TaskEvent;
+
+import javax.swing.*;
+
 public class Test {
     private int test;
     private int bg;
@@ -17,4 +21,26 @@ public class Test {
     public void setTest(int test) {
         this.test = test;
     }
+
+/*  test lancement parallel utile pour la methode load
+    public static void main(String[] args) {
+        JProgressBar jb = new JProgressBar();
+        Thread t= new Thread(){
+            @Override
+            public void run() {
+                for(int i = 0 ; i<100 ; i++) {
+                    try {
+                        sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(i);
+                }
+            }
+        };
+        t.start();
+        System.out.println("here");
+        //Thread.sleep(10000);
+    }
+    */
 }
