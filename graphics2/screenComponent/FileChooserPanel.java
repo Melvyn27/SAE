940e2eedf60632ec0b1dba57da4e0507f33682b5
Guilhem Cros <guilhem.cros.38@gmail.com>
2022-05-20 15:39:46 +0200

    init

--- graphics2/screenComponent/FileChooserPanel.java
+++ graphics2/screenComponent/FileChooserPanel.java
@@ -1,7 +1,30 @@
 package SAE.graphics2.screenComponent;
 
-public class FileChooserPanel {
+import javax.swing.*;
+import java.awt.*;
+
+public class FileChooserPanel extends JPanel {
     //todo faire l'affichage et la gestion parallèle
+    JFileChooser fileChooser = new JFileChooser();
+    JProgressBar loadSiteBar = new JProgressBar();
+    JProgressBar loatDestBar = new JProgressBar();
+    public FileChooserPanel(){
+
+
+
+
+    }
+    private void init(){
+
+        fileChooser.setControlButtonsAreShown(false);
+        add(fileChooser, BorderLayout.CENTER);
+
+
+
+
+
+    }
+
 
 
 
