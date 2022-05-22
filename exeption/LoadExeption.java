ea58407671d66e775d3976b446076c8c45bc2c05
guilhem <guilhem.cros.38@gmail.com>
2022-05-22 09:58:37 +0200

    changement du style d'affichage

--- exeption/LoadExeption.java
+++ exeption/LoadExeption.java
@@ -1,20 +1,23 @@
 package SAE.exeption;
 
+import SAE.graphics2.screenComponent.FileLoaderPanel;
 import SAE.graphics2.threadedMotors.ChargementParallele;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class LoadExeption extends Exception {
     //fixme adapter les exeption a la nouvelle interface
     JFrame frame;
-    public LoadExeption(ChargementParallele target) {
+    public LoadExeption(FileLoaderPanel target) {
         JOptionPane.showMessageDialog(frame,"erreur de chargement","erreur de chargement",JOptionPane.WARNING_MESSAGE);
     }
-    public LoadExeption(ChargementParallele target,String line) {
-        target.setTargetMessage("erreur de chargement a la ligne "+line);
+    public LoadExeption(FileLoaderPanel target,String line) {
+        target.information.setForeground(new Color(255,0,0));
+        target.information.setText("erreur de chargement a la ligne "+line);
     }
-    public LoadExeption(ChargementParallele target,String line, String site) {
-
-        target.setTargetMessage("le site "+site+" est dupliqué a la ligne "+line);
+    public LoadExeption(FileLoaderPanel target,String line, String site) {
+        target.information.setForeground(new Color(255,0,0));
+        target.information.setText("le site "+site+" est dupliqué a la ligne "+line);
     }
 }
--- exeption/VerificationExeption.java
+++ exeption/VerificationExeption.java
@@ -1,19 +1,22 @@
 package SAE.exeption;
 
+import SAE.graphics2.screenComponent.FileLoaderPanel;
 import SAE.graphics2.threadedMotors.ChargementParallele;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class VerificationExeption extends Exception{
     //fixme adapter les exeption a la nouvelle interface
     JFrame frame;
-    public VerificationExeption(ChargementParallele target){
+    public VerificationExeption(FileLoaderPanel target){
         JOptionPane.showMessageDialog(frame,"fichier incomplet","erreur de chargement",JOptionPane.WARNING_MESSAGE);
     }
-    public VerificationExeption(ChargementParallele target, String site){
-        target.setTargetMessage("le site "+site+" est introuvable");
+    public VerificationExeption(FileLoaderPanel target, String site){
+        target.information.setForeground(new Color(255,0,0));
+        target.information.setText("le site "+site+" est introuvable");
     }
-    public VerificationExeption(ChargementParallele target,String site1,String site2){
+    public VerificationExeption(FileLoaderPanel target,String site1,String site2){
         JOptionPane.showMessageDialog(frame,"la route reliant le site "+site1+" au site "+site2+" est incomplete","erreur de chargement",JOptionPane.WARNING_MESSAGE);
     }
 }
