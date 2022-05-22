55dbb9ef52be90d41bcf8b1cf66142c58effbb8f
guilhem <guilhem.cros.38@gmail.com>
2022-05-22 10:39:36 +0200

    ajout d'une fonctionnalité

--- exeption/LoadExeption.java
+++ exeption/LoadExeption.java
@@ -9,10 +9,11 @@ import java.awt.*;
 public class LoadExeption extends Exception {
     //fixme adapter les exeption a la nouvelle interface
     JFrame frame;
-    public LoadExeption(FileLoaderPanel target) {
-        JOptionPane.showMessageDialog(frame,"erreur de chargement","erreur de chargement",JOptionPane.WARNING_MESSAGE);
+    public LoadExeption(FileLoaderPanel target,String message) {
+        target.information.setForeground(new Color(255,0,0));
+        target.information.setText(message);
     }
-    public LoadExeption(FileLoaderPanel target,String line) {
+    public LoadExeption(FileLoaderPanel target,int line) {
         target.information.setForeground(new Color(255,0,0));
         target.information.setText("erreur de chargement a la ligne "+line);
     }
