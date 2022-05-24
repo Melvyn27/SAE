39cf08362fd723d2b72dc7dc8f19886a3ff45642
Guilhem Cros <guilhem.cros.38@gmail.com>
2022-05-24 09:43:57 +0200

    modification de la methode d'affichage

--- graphics2/screenComponent/SettingsPanel.java
+++ graphics2/screenComponent/SettingsPanel.java
@@ -4,28 +4,50 @@ import SAE.graphics2.Screen;
 import SAE.graphics2.format.LogFormat;
 
 import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
 
 public class SettingsPanel extends JPanel {
     Screen screen;
 
+    JPanel global = new JPanel(new GridLayout(0,2,10,10));
+
     public SettingsPanel(Screen screen){
         this.screen=screen;
+        this.setLayout(new FlowLayout());
         init();
     }
 
     void init(){
+
+
         JComboBox<LogFormat> choosFormat= new JComboBox<>();
         choosFormat.addItem(LogFormat.simple);
         choosFormat.addItem(LogFormat.none);
         choosFormat.addItem(LogFormat.all);
         choosFormat.addActionListener(l->screen.getLog().setFormat((LogFormat)choosFormat.getSelectedItem()));
         choosFormat.setToolTipText("/!\\ 'all' peut poser des problème d'affichage");
-        add(choosFormat);
+        //add(choosFormat);
+
+        addSettings("format d'affichage",choosFormat);
+        addSettings("test",new JButton("test"));
 
 
 
+        add(global);
     }
 
+    void addSettings(String legends,JComponent component){
+        JPanel p1 = new JPanel(new FlowLayout(FlowLayout.LEFT));
+
+        p1.setBackground(new Color(50,50,50));
+
+        p1.add(new JLabel(legends));
+        p1.add(component);
+
+        global.add(p1);
+
+    }
 
 
 
