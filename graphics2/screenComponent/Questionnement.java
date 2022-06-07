15744b975b8e08dab9cdea862b9073dd74d33443
Guilhem Cros <guilhem.cros.38@gmail.com>
2022-06-07 12:07:25 +0200

    netoyage du code

--- graphics2/screenComponent/Questionnement.java
+++ graphics2/screenComponent/Questionnement.java
@@ -1,10 +1,7 @@
 package SAE.graphics2.screenComponent;
 
 import SAE.graphics2.Screen;
-import SAE.graphics2.screenComponent.questionComponent.ListerLesSitePanel;
-import SAE.graphics2.screenComponent.questionComponent.PathPanel;
-import SAE.graphics2.screenComponent.questionComponent.VoisinDePanel;
-import SAE.graphics2.screenComponent.questionComponent.VoisinProchePanel;
+import SAE.graphics2.screenComponent.questionComponent.*;
 import SAE.map.Site;
 
 import javax.swing.*;
@@ -19,16 +16,19 @@ Screen screen;
 
 VoisinDePanel voisin;
 VoisinProchePanel voisinProche;
+ComparePanel comparePanel ;
 
     public Questionnement(Screen screen){
         this.screen=screen;
         voisin = new VoisinDePanel(screen);
         voisinProche = new VoisinProchePanel(screen);
+        comparePanel = new ComparePanel(screen);
 
         addTab("voisin",voisin);
         addTab("lister",new ListerLesSitePanel(screen));
-        addTab("chemin",new PathPanel(screen));
+        //addTab("chemin",new PathPanel(screen));
         addTab("voisin a deux",voisinProche);
+        addTab("comparer",comparePanel);
 
 
 
@@ -38,7 +38,7 @@ VoisinProchePanel voisinProche;
     public void updateChoix(ArrayList<Site> s){
         voisin.updateChoix(s);
         voisinProche.updateChoix(s);
-
+        comparePanel.updateChoix(s);
 
     }
 }
